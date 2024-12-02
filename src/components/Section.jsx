70f320b6cb386e075fa63d336912b7def76b4fd8
Vladislav <baiborodov333@mail.ru>
2024-12-02 18:19:31 +0500

    hero-section-finish

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -18,10 +18,7 @@ const Section = ({
         className || ""
       }`}
     >
-      {children && typeof children === "string" ? children : "Section"}
-
-      <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none mb:block lg:left-7.5 xl:left-10" />
-      <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none mb:block lg:right-7.5 xl:right-10" />
+      {children || "Section"}
 
       {crosses && (
         <>
